d6472aa20d7e5b619eecc3d7563eb7ee261ce44d
satish <satishkonduru0225@gmail.com>
2024-10-30 16:11:54 +0530

    Banner Completed

---
+++ src/app/pages/banner/banner.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.css',
+})
+export class BannerComponent implements OnInit {
+  ngOnInit(): void {
+    const options = {
+      strings: [
+        'UI/UX Designer',
+        'Angular Developer',
+        'Backend Developer',
+        'Educator',
+        'Youtuber',
+        'Trainer',
+        'Graphics Designer',
+        'Fullstack Developer',
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1000,
+      loop: true,
+    };
+
+    const typed = new Typed('.typedText', options);
+  }
+}
